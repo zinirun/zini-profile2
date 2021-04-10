cc325b5171a2c9da6fa9c6ff0e9e7acd9ddf112f
zinirun <heojj97@naver.com>
2021-04-10 18:47:30 +0900

    modify: prod sets

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -5,10 +5,10 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains="zini.run"
+domains="YOUR_DOMAIN"
 rsa_key_size=4096
 data_path="../certbot"
-email="zrunkr@gmail.com" # Adding a valid address is strongly recommended
+email="YOUR_EMAIL" # Adding a valid address is strongly recommended
 staging=0 # Set to 1 if you're testing your setup to avoid hitting request limits
 
 if [ -d "$data_path" ]; then
