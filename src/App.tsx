e34093f27763866701376486c8f80602b0767566
zinirun <heojj97@naver.com>
2021-04-21 18:40:15 +0900

    modify: remove div

--- src/App.tsx
+++ src/App.tsx
@@ -41,121 +41,113 @@ function App() {
                         <Chip className={classes.socialChip} label={social.email} size="small" />
                     </Grid>
                 </Grid>
-                <div>
-                    <p className={classes.sectionTitle}>Skills</p>
-                    <p className={classes.itemTitle}>Backend</p>
-                    {skills.backend.map((skill, idx) => (
-                        <Chip
-                            avatar={
-                                skill.avatarSrc ? (
-                                    <Avatar alt={skill.name} src={skill.avatarSrc} />
-                                ) : (
-                                    <></>
-                                )
-                            }
-                            className={classes.skillChip}
-                            key={idx}
-                            label={skill.name}
-                            variant="outlined"
-                        />
-                    ))}
+                <p className={classes.sectionTitle}>Skills</p>
+                <p className={classes.itemTitle}>Backend</p>
+                {skills.backend.map((skill, idx) => (
+                    <Chip
+                        avatar={
+                            skill.avatarSrc ? (
+                                <Avatar alt={skill.name} src={skill.avatarSrc} />
+                            ) : (
+                                <></>
+                            )
+                        }
+                        className={classes.skillChip}
+                        key={idx}
+                        label={skill.name}
+                        variant="outlined"
+                    />
+                ))}
 
-                    <p className={classes.itemTitle}>Frontend</p>
-                    {skills.frontend.map((skill, idx) => (
-                        <Chip
-                            avatar={
-                                skill.avatarSrc ? (
-                                    <Avatar alt={skill.name} src={skill.avatarSrc} />
-                                ) : (
-                                    <></>
-                                )
-                            }
-                            className={classes.skillChip}
-                            key={idx}
-                            label={skill.name}
-                            variant="outlined"
-                        />
-                    ))}
+                <p className={classes.itemTitle}>Frontend</p>
+                {skills.frontend.map((skill, idx) => (
+                    <Chip
+                        avatar={
+                            skill.avatarSrc ? (
+                                <Avatar alt={skill.name} src={skill.avatarSrc} />
+                            ) : (
+                                <></>
+                            )
+                        }
+                        className={classes.skillChip}
+                        key={idx}
+                        label={skill.name}
+                        variant="outlined"
+                    />
+                ))}
 
-                    <p className={classes.itemTitle}>Database</p>
-                    {skills.database.map((skill, idx) => (
-                        <Chip
-                            avatar={
-                                skill.avatarSrc ? (
-                                    <Avatar alt={skill.name} src={skill.avatarSrc} />
-                                ) : (
-                                    <></>
-                                )
-                            }
-                            className={classes.skillChip}
-                            key={idx}
-                            label={skill.name}
-                            variant="outlined"
-                        />
-                    ))}
+                <p className={classes.itemTitle}>Database</p>
+                {skills.database.map((skill, idx) => (
+                    <Chip
+                        avatar={
+                            skill.avatarSrc ? (
+                                <Avatar alt={skill.name} src={skill.avatarSrc} />
+                            ) : (
+                                <></>
+                            )
+                        }
+                        className={classes.skillChip}
+                        key={idx}
+                        label={skill.name}
+                        variant="outlined"
+                    />
+                ))}
 
-                    <p className={classes.itemTitle}>Infrastructure</p>
-                    {skills.infrastructure.map((skill, idx) => (
-                        <Chip
-                            avatar={
-                                skill.avatarSrc ? (
-                                    <Avatar alt={skill.name} src={skill.avatarSrc} />
-                                ) : (
-                                    <></>
-                                )
-                            }
-                            className={classes.skillChip}
-                            key={idx}
-                            label={skill.name}
-                            variant="outlined"
-                        />
-                    ))}
-                </div>
+                <p className={classes.itemTitle}>Infrastructure</p>
+                {skills.infrastructure.map((skill, idx) => (
+                    <Chip
+                        avatar={
+                            skill.avatarSrc ? (
+                                <Avatar alt={skill.name} src={skill.avatarSrc} />
+                            ) : (
+                                <></>
+                            )
+                        }
+                        className={classes.skillChip}
+                        key={idx}
+                        label={skill.name}
+                        variant="outlined"
+                    />
+                ))}
 
-                <div>
-                    <p className={classes.sectionTitle}>Projects</p>
-                    {projects.map((item, idx) => (
-                        <li key={idx} className={classes.itemList}>
-                            <Link href={item.link} color="inherit" target="_blank">
-                                <span className={classes.itemListText}>{item.name}</span>
-                            </Link>
-                        </li>
-                    ))}
-                </div>
+                <p className={classes.sectionTitle}>Projects</p>
+                {projects.map((item, idx) => (
+                    <li key={idx} className={classes.itemList}>
+                        <Link href={item.link} color="inherit" target="_blank">
+                            <span className={classes.itemListText}>{item.name}</span>
+                        </Link>
+                    </li>
+                ))}
 
-                <div>
-                    <p className={classes.sectionTitle}>Opensources</p>
-                    {opensources.map((item, idx) => (
-                        <li key={idx} className={classes.itemList}>
-                            <Link href={item.link} color="inherit" target="_blank">
-                                <span className={classes.itemListText}>{item.name}</span>
-                            </Link>
-                        </li>
-                    ))}
-                </div>
+                <p className={classes.sectionTitle}>Opensources</p>
+                {opensources.map((item, idx) => (
+                    <li key={idx} className={classes.itemList}>
+                        <Link href={item.link} color="inherit" target="_blank">
+                            <span className={classes.itemListText}>{item.name}</span>
+                        </Link>
+                    </li>
+                ))}
 
-                <div>
-                    <p className={classes.sectionTitle}>Education</p>
-                    <p className={classes.itemTitle}>
-                        {education.highName}{' '}
-                        <span className={classes.smallItalicText}>{education.highLocation}</span>
-                    </p>
-                    <p className={classes.itemTitle}>
-                        {education.univName}{' '}
-                        <span className={classes.smallItalicText}>{education.univDept},</span>{' '}
-                        <span className={classes.smallItalicText}>{education.univLocation}</span>
-                    </p>
-                    <p className={classes.itemTitle}>Related Courses Taken</p>
-                    {education.grades.map((item, idx) => (
-                        <Chip
-                            avatar={<Avatar className={classes.gradeAvatar}>{item.grade}</Avatar>}
-                            className={classes.gradeChip}
-                            key={idx}
-                            label={item.subject}
-                            variant="outlined"
-                        />
-                    ))}
-                </div>
+                <p className={classes.sectionTitle}>Education</p>
+                <p className={classes.itemTitle}>
+                    {education.highName}{' '}
+                    <span className={classes.smallItalicText}>{education.highLocation}</span>
+                </p>
+                <p className={classes.itemTitle}>
+                    {education.univName}{' '}
+                    <span className={classes.smallItalicText}>{education.univDept},</span>{' '}
+                    <span className={classes.smallItalicText}>{education.univLocation}</span>
+                </p>
+                <p className={classes.itemTitle}>Related Courses Taken</p>
+                {education.grades.map((item, idx) => (
+                    <Chip
+                        avatar={<Avatar className={classes.gradeAvatar}>{item.grade}</Avatar>}
+                        className={classes.gradeChip}
+                        key={idx}
+                        label={item.subject}
+                        variant="outlined"
+                    />
+                ))}
             </Container>
         </div>
     );
